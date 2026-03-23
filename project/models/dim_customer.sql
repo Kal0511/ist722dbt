57c7fffe3182e451f497298ee180cd4a3c98a3cc
Kaumil Patel <kpatel80@g.syr.edu>
2026-03-23 00:32:37 +0000

    merge

---
+++ project/models/dim_customer.sql
@@ -0,0 +1,22 @@
+with stg_customers as (
+    select * from {{ source('fudgemart_V3','fm_customers')}}
+),
+stg_accounts as (
+    select * from {{ source('fudgeflix_V3','ff_accounts')}}
+)
+select  {{ dbt_utils.generate_surrogate_key(['c.customer_id','a.account_id']) }} as customer_key, 
+    c.customer_id, 
+    a.account_id,
+    COALESCE(c.customer_email, a.account_email) as customer_email,
+    COALESCE(c.customer_firstname, a.account_firstname) as customer_firstname,
+    COALESCE(c.customer_lastname, a.account_lastname) as customer_lastname,
+    COALESCE(c.customer_address, a.account_address) as customer_address,
+    c.customer_city,
+    c.customer_state,
+    COALESCE(c.customer_zip, a.account_zipcode) as customer_zip,
+    c.customer_phone,
+    c.customer_fax,
+    a.account_plan_id,
+    a.account_opened_on
+from stg_customers c
+full join stg_accounts a on c.customer_id = (a.account_id-10000)
